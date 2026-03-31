ee320d6a31c217fe6802bd842624072f7de969b7
tubagusfirdaus06 <158448258+tubagusfirdaus06@users.noreply.github.com>
2026-03-31 12:46:56 +0700

    git add . git commit -m "fix listen duplicate" git push

--- server.js
+++ server.js
@@ -13,12 +13,6 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-  console.log("🚀 Server jalan di port", PORT);
-});
-
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static("public"));
@@ -190,8 +184,10 @@ app.get("/check/:id", (req, res) => {
 
 /* ========================= */
 
-server.listen(3000, () => {
-  console.log("🚀 Server jalan di http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log("🚀 Server jalan di port", PORT);
 });
 
 /* ========================= */
