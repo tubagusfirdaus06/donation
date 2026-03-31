97b3f92d7a6a20bc2f0193386708660c335a42eb
tubagusfirdaus06 <158448258+tubagusfirdaus06@users.noreply.github.com>
2026-03-31 12:32:46 +0700

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log("🚀 Server jalan di port", PORT);
+});
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static("public"));
